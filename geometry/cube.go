b5a7bb9eaedbfba09cbd1b4f7b6e4a4753abd42f
Mohit Bisht <mohitbishtdev@gmail.com>
2020-08-17 02:14:15 -0400

    Add error check for CubeVolumne()

--- geometry/cube.go
+++ geometry/cube.go
@@ -1,6 +1,11 @@
 package geometry
 
+import "errors"
+
 // CubeVolume calculates the volume of a cube where n is the cube length
-func CubeVolume(n int) int {
-	return n * n * n
+func CubeVolume(n int) (int, error) {
+	if n == 0 {
+		return 0, errors.New("Zero is not allowed")
+	}
+	return n * n * n, nil
 }
